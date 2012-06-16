2ef68df688765b42c9b0830e5972c7c2856b815b
Rickard Hansson <Rickard.Hansson@cybercom.com>
2012-06-16 17:55:09 +0200

    iPhone
    first

---
+++ app/app/AppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppAppDelegate.h
+//  app
+//
+//  Created by Rickard Hansson on 2012-06-16.
+//  Copyright (c) 2012 cybercom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ app/app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  app
+//
+//  Created by Rickard Hansson on 2012-06-16.
+//  Copyright (c) 2012 cybercom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppAppDelegate class]));
+    }
+}
